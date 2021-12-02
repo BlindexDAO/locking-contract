21b1177929e6b8928bb0b10e1d3eeb8e5e5a0b6a
Or Kazaz <or.kazaz89@gmail.com>
2021-12-02 22:38:57 +0200

    FIxed test script

--- contracts/BDVestingContract.sol
+++ contracts/BDVestingContract.sol
@@ -7,12 +7,7 @@ import "@openzeppelin/contracts/finance/VestingWallet.sol";
 // 1. release() - limit so only the beneficiaryAddress could execute it
 // 2. implement release() which recieves an amount parameter to release specific amount
 contract BDVestingContract is VestingWallet {
-    address beneficiaryAddress =
-        address(0x1d827461beC8BC392E38d48357E065dfceC23553);
-    uint64 startTimestamp = uint64(block.timestamp);
-    uint64 durationSeconds = 3600;
-
-    constructor()
+    constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds)
         VestingWallet(beneficiaryAddress, startTimestamp, durationSeconds)
     {}
 
