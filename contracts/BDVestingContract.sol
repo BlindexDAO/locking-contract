829b7346a29af21ef0a9f6726e913cd7f74e1e26
Tom Keidar <tom.keidar@portis.io>
2021-12-02 21:22:02 +0200

    test the original VestingWallet contract

--- contracts/BDVestingContract.sol
+++ contracts/BDVestingContract.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/finance/VestingWallet.sol";
 
+// 1. release() - limit so only the beneficiaryAddress could execute it
+// 2. implement release() which recieves an amount parameter to release specific amount
 contract BDVestingContract is VestingWallet {
     address beneficiaryAddress =
         address(0x1d827461beC8BC392E38d48357E065dfceC23553);
@@ -14,6 +16,27 @@ contract BDVestingContract is VestingWallet {
         VestingWallet(beneficiaryAddress, startTimestamp, durationSeconds)
     {}
 
-    // 1. release() - limit so only the beneficiaryAddress could execute it
-    // 2. implement release() which recieves an amount parameter to release specific amount
+    function released(address token)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return VestingWallet.released(token);
+    }
+
+    function release(address token) public virtual override {
+        return VestingWallet.release(token);
+    }
+
+    function vestedAmount(address token, uint64 timestamp)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return VestingWallet.vestedAmount(token, timestamp);
+    }
 }
