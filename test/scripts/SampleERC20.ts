56cbda34c617b9a852a0a2087dffbdfdd2d5cfe4
Or Kazaz <or.kazaz89@gmail.com>
2021-12-06 09:26:29 +0200

    Kazaz/debt finish js to ts (#25)
    * Added typechain support for mocha context

--- test/scripts/SampleERC20.ts
+++ test/scripts/SampleERC20.ts
@@ -1,6 +1,7 @@
+import { Contract } from "@ethersproject/contracts";
 import { ethers } from "hardhat";
 
-async function deployErc20Contract(totalSupply: number, name: string = "Sample Token", symbol: string = "SMPL") {
+async function deployErc20Contract(totalSupply: number, name: string = "Sample Token", symbol: string = "SMPL"): Promise<Contract> {
   const factory = await ethers.getContractFactory("SampleERC20");
   const erc20Contract = await factory.deploy(name, symbol, totalSupply);
   await erc20Contract.deployTransaction.wait();
