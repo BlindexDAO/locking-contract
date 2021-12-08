b00c76cfe5cf02200130f0f41f800c8d7d46bbcf
kazazor <kazazor@users.noreply.github.com>
2021-12-08 08:48:53 +0000

    Prettified code and generated docs!

--- deploy/1_deploy_locking_contract.ts
+++ deploy/1_deploy_locking_contract.ts
@@ -7,8 +7,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const [deployer, treasury, firstBeneficiary, secondBeneficiary, thirdBeneficiary] = await hre.ethers.getSigners();
 
   const startTimestamp = moment().unix();
-  const duration = moment.duration(2, 'years').asSeconds();
-  const cliffSeconds = moment.duration(1, 'year').asSeconds();
+  const duration = moment.duration(2, "years").asSeconds();
+  const cliffSeconds = moment.duration(1, "year").asSeconds();
 
   const BDLockingContract = await hre.ethers.getContractFactory("BDLockingContract");
   const lockingContract = await BDLockingContract.connect(deployer).deploy(
