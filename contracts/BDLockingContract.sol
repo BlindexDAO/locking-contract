6cd271f0bb63fab9f84ba8a86ace48970e63307f
Or Kazaz <or.kazaz89@gmail.com>
2021-12-16 13:52:48 +0200

    Remove Math.min

--- contracts/BDLockingContract.sol
+++ contracts/BDLockingContract.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
