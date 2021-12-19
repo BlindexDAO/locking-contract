3295d514964e11aa420b3601cacfa7a5163f549d
Tom Keidar <tom.keidar@portis.io>
2021-12-19 18:25:23 +0200

    remove BDX transfer from contract deployment script

--- deploy/1_deploy_locking_contract.ts
+++ deploy/1_deploy_locking_contract.ts
@@ -1,12 +1,9 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import moment from "moment";
-import { ethers } from "ethers";
-import BDXContract from "../abis/BDXContract";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log("Deploy BDLockingContract...");
-  const bdxContractAddress = process.env.BDX_CONTRACT_ADDRESS!; // Fill BDX contract address
   const [deployer, treasury] = await hre.ethers.getSigners(); // TODO: Set treasury and deployer before deployment to production
 
   const startTimestamp = moment().unix();
@@ -21,14 +18,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 
   console.log("BDLockingContract deployed to:", deployedLockingContract.address);
-
-  // TODO: Should we run it here or by the blindex scripts?
-  const bdxAmount = ethers.utils.parseEther("3150000"); // 21M tokens * 15%
-  console.log(`Transfer ${bdxAmount} BDX tokens from the treasury to the locking contract.`);
-  const bdx = await hre.ethers.getContractAt(BDXContract, bdxContractAddress);
-  const tx = await bdx.connect(treasury).transfer(deployedLockingContract.address, bdxAmount);
-  await tx.wait();
-
   return true;
 };
 func.id = __filename;
