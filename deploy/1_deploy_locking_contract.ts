145044d810327b95581a2e76048935879255c4e6
Tom Keidar <tom.keidar@portis.io>
2021-12-21 10:05:49 +0200

    set beneficiaries wallet addresses

--- deploy/1_deploy_locking_contract.ts
+++ deploy/1_deploy_locking_contract.ts
@@ -9,7 +9,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const startTimestamp = moment().unix();
   const durationSeconds = moment.duration(2, "years").asSeconds();
   const cliffSeconds = moment.duration(1, "year").asSeconds();
-  const beneficiaries: string[] = ["0xd7ED3F0ff6823e000e08E79C340697545c5925a3", "0xb34B2168D5D869F18A4Fd63eC11287F72c302251"]; // TODO: Fill addresses
+  const beneficiaries = [
+    "0xd7ED3F0ff6823e000e08E79C340697545c5925a3",
+    "0xb34B2168D5D869F18A4Fd63eC11287F72c302251",
+    "0x1a99B0B8E5b4c02bB3C682355288c3d7DAEA227B",
+  ];
 
   const deployedLockingContract = await hre.deployments.deploy("BDLockingContract", {
     from: deployer.address,
